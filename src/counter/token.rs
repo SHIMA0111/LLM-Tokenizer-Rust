f95710c41dc3a6234674382dd9a3ab0debddb6c6
SHIMA0111 <shima@little-tabby.com>
2024-04-08 04:44:32 +0900

    Add README and LICENSE

--- src/counter/token.rs
+++ src/counter/token.rs
@@ -1,29 +1,10 @@
-/// This source is copied from master branch on https://github.com/openai/tiktoken/blob/main/src/lib.rs.
-/// at April 7, 2024.
+/// This source is copied from master branch on https://github.com/openai/tiktoken/
+/// at April 7, 2024, following the MIT license of the original project.
+/// (The exact file is https://github.com/openai/tiktoken/blob/main/src/lib.rs)
 ///
-/// The original license is MIT as the below.
-///
-/// MIT License
-///
-/// Copyright (c) 2022 OpenAI, Shantanu Jain
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in all
-/// copies or substantial portions of the Software.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-/// SOFTWARE.
+/// Please note this source file was edited by this(langcat) project
+/// so some parts doesn't match the original library.
+/// YOU MUST NEVER COMPLAIN ANYTHING OF LOSS BY THIS CODE TO THE ORIGINAL SOURCE AUTHOR.
 
 use std::collections::HashSet;
 use std::num::NonZeroU64;
